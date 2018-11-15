60924c57117daa93b9478877f17cbb13a990286a
TedWildenradt <tedkwildenradt@gmail.com>
2018-11-15 11:48:10 -0800

    Add login form

---
+++ music_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+    if @user
+      login(@user)
+    else
+      redirect_to new_session_url
+    end
+  end
+
+  def destroy
+    logout(user)
+    redirect_to new_session_url
+  end
+
+  def new
+    render :new
+  end
+
+end
--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
 
-  def index
-    
-  end
 
   def create
+    @user = User.new(user_params)
+    debugger
+    if @user.save
+      login(@user)
+    else
+      flash.now[:errors]. << @user.errors.full_messages
+      render :new
+    end
 
+  end
+
+  def new
+    @user = User.new
+    render :new
   end
 
   def show
 
   end
 
-  def new
-
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
